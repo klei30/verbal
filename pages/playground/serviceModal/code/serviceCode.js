216600fb4efa588dd40f8f319475e8f4a3a0cf1f
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-12-03 13:20:45 +0100

    Delete pages directory

--- pages/playground/serviceModal/code/serviceCode.js
+++
@@ -1,6 +0,0 @@
-import './serviceCode.css';
-import React from 'react';
-
-export default function Code({code}) {
-  return <pre id="service-code">{code}</pre>;
-}
--- pages/smallScreen/deepChatLogo.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function DeepChatLogo() {
-  return <img id="small-screen-deep-chat-logo" src="img/deep-chat-small-screen-logo.svg" />;
-}
