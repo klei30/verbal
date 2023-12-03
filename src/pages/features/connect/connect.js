226c78b096f98ed6591cb49f0b862869337b3335
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-12-03 19:36:58 +0100

    Update connect.js

--- src/pages/features/connect/connect.js
+++ src/pages/features/connect/connect.js
@@ -7,8 +7,9 @@ function RightPanel() {
   return (
     <div id="connect-right-panel" className="feature-panel">
       <div id="connect-text">
-        verbal can connect to any API. Use it to communicate with popular AI providers directly from the browser or
-        configure it to connect to your own servers.
+       "Verbal: The Gateway to Seamless Integration. With Verbal, the possibilities are limitless. Connect effortlessly to a wide array of APIs, unlocking the potential to communicate with leading AI providers right from your browser. Or, take control and customize your experience by configuring Verbal to link seamlessly with your own server systems. Whether you're tapping into external AI powerhouses or leveraging your in-house solutions, Verbal is your bridge to a smarter, more connected world."
+
+
       </div>
     </div>
   );
@@ -17,33 +18,30 @@ function RightPanel() {
 function LeftPanel() {
   return (
     <div id="connect-left-panel" className="feature-panel">
-      <DeepChatBrowser
-        demo={true}
-        initialMessages={[
-          {text: 'Where do I start?', role: 'user'},
-          {text: 'Check Docs on how to install this component.', role: 'ai'},
-          {text: 'Can it connect to my custom API?', role: 'user'},
-          {text: 'It sure can! Check the Service section in Docs.', role: 'ai'},
-        ]}
-        style={{
-          borderRadius: '10px',
-          boxShadow: '0 .5rem 1rem 0 rgba(44, 51, 73, .1)',
-          border: '1px solid #ededed',
-        }}
-        messageStyles={{
-          default: {
-            user: {
-              bubble: {
-                backgroundColor: '#397cd8',
-              },
-            },
-          },
-        }}
-        stream={true}
-      ></DeepChatBrowser>
+      <div className="iframe-container" style={{ display: 'flex', justifyContent: 'center' }}>
+        <iframe
+          src="https://llama2.kleyaliaj.repl.co/"
+          title="Chatbot"
+          loading="lazy"
+          style={{
+            width: '60%',
+            height: '500px',
+            borderRadius: '10px',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            border: '1px solid #ccc',
+            marginLeft: '10%', // Adjust this to control how far to the right the iframe appears
+            marginRight: '0',
+          }}
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
+
+
+
 
 export default function Connect() {
   return (
